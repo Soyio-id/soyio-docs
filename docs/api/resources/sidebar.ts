2aaefdd80432764e7a93142723409225c0cbd4e0
Jazmine Maldonado <jazminemf@gmail.com>
2024-12-19 22:55:25 -0300

    docs: remove intro page

--- docs/api/resources/sidebar.ts
+++ docs/api/resources/sidebar.ts
@@ -3,10 +3,6 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebar: SidebarsConfig = {
   apisidebar: [
     {
-      type: "doc",
-      id: "api/resources/soyio-api",
-    },
-    {
       type: "category",
       label: "Disclosure requests",
       items: [
