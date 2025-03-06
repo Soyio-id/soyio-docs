70a565319ce3fc4c6f09889f206d8a3a356a8197
Ignacio Acosta <ignacioa@soyio.id>
2025-03-06 12:38:24 -0300

    chore(intercom): configure messenger

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,6 +81,28 @@ const config: Config = {
     ],
     './lib/soyioDocs/index.ts',
     'docusaurus-plugin-fathom',
+    async function intercom() {
+      return {
+        name: 'intercom',
+        injectHtmlTags: () => {
+          return {
+            headTags: [
+              {
+                tagName: 'link',
+                attributes: {
+                  rel: 'preconnect',
+                  href: 'https://widget.intercom.io',
+                },
+              },
+              {
+                tagName: 'script',
+                innerHTML: `window.intercomSettings={app_id:'t56kvykx'};(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/t56kvykx';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`,
+              },
+            ],
+          };
+        },
+      };
+    },
   ],
   themeConfig: {
     docs: {
