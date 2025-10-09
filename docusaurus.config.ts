fa7cc901ff12c6655f4d15b47d91b7891ebb02df
Jazmine Maldonado <jazminemf@gmail.com>
2025-10-09 19:11:02 -0300

    fix: try to fix build error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,6 +8,7 @@ import type * as FathomAnalyticsPlugin from 'docusaurus-plugin-fathom';
 const config: Config = {
   markdown: {
     mermaid: true,
+    hooks: { onBrokenMarkdownLinks: 'warn'},
   },
   title: 'Soyio Docs',
   tagline: 'Documentación de la infraestructura de la privacidad digital',
@@ -25,7 +26,6 @@ const config: Config = {
   projectName: 'soyio-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
